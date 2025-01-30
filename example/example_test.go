e51aa0b4ab4e4c8d8d2f13d8e2ede2fe22629944
LINCKODE <linckode@gmail.com>
2025-01-30 21:44:49 +0200

    Initial implementation of Signer and LiveServiceClient utility objects.

---
+++ example/example_test.go
@@ -0,0 +1,39 @@
+package example
+
+/*func TestPerformSimpleTransaction(t *testing.T) {
+
+	sourceAddr := ""
+	sourceSeed := ""
+
+	destinationAddr := ""
+	amount := int64(0)
+
+	_, err := PerformSimpleTransaction(sourceAddr, sourceSeed, destinationAddr, amount)
+	if err != nil {
+		t.Fatal(errors.Wrap(err, "performing simple transaction"))
+	}
+
+}*/
+
+/*func TestPerformSendManyTransaction(t *testing.T) {
+
+	sourceAddr := ""
+	sourceSeed := ""
+
+	sendManyTransfers := []types.SendManyTransfer{
+		{
+			AddressID: "",
+			Amount:    0,
+		},
+		{
+			AddressID: "",
+			Amount:    0,
+		},
+	}
+
+	_, err := PerformSendManyTransaction(sourceAddr, sourceSeed, sendManyTransfers)
+	if err != nil {
+		t.Fatal(errors.Wrap(err, "performing send many transaction"))
+	}
+
+}*/
