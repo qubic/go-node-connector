b8d149177a34067e68da4d2316c6b8c0b688a63c
mio <180114449+qubicmio@users.noreply.github.com>
2025-12-04 19:36:48 +0000

    Replace test data with real data from testnet

--- types/ipos_test.go
+++ types/ipos_test.go
@@ -10,24 +10,36 @@ import (
 )
 
 func TestIpos_UnmarshallFromReader(t *testing.T) {
-	header, err := base64.StdEncoding.DecodeString("FAAAQeKmLXg=")
+	activeIpoHeader, err := base64.StdEncoding.DecodeString("FAAAQehMlcw=")
 	require.NoError(t, err)
 
-	payload, err := base64.StdEncoding.DecodeString("KgAAAFRFU1QAAAAA")
+	activeIpoPayload, err := base64.StdEncoding.DecodeString("EwAAAFExOTAxAAAA")
+	require.NoError(t, err)
+
+	activeIpo2Header, err := base64.StdEncoding.DecodeString("FAAAQehMlcw=")
+	require.NoError(t, err)
+
+	activeIpo2Payload, err := base64.StdEncoding.DecodeString("FAAAAFExOTAyAAAA")
 	require.NoError(t, err)
 
 	endResponseHeader, err := base64.StdEncoding.DecodeString("CAAAI/PqVtQ=")
 	require.NoError(t, err)
 
-	message := append(header, payload...)
+	message := make([]byte, 0)
+	message = append(message, activeIpoHeader...)
+	message = append(message, activeIpoPayload...)
+	message = append(message, activeIpo2Header...)
+	message = append(message, activeIpo2Payload...)
 	message = append(message, endResponseHeader...)
 
 	var result Ipos
 	err = result.UnmarshallFromReader(bytes.NewReader(message))
 	require.NoError(t, err)
-	assert.Len(t, result, 1)
-	assert.Equal(t, 42, int(result[0].ContractIndex))
-	assert.Equal(t, []byte("TEST\x00\x00\x00\x00"), result[0].AssetName[:])
+	assert.Len(t, result, 2)
+	assert.Equal(t, 19, int(result[0].ContractIndex))
+	assert.Equal(t, []byte("Q1901\x00\x00\x00"), result[0].AssetName[:])
+	assert.Equal(t, 20, int(result[1].ContractIndex))
+	assert.Equal(t, []byte("Q1902\x00\x00\x00"), result[1].AssetName[:])
 }
 
 func TestIpo_UnmarshallBinary(t *testing.T) {
